61dd94b19b7ef31d3d520fd40c03d81cb9a2c2b6
Chengfei Zhang <chengfeizh@gmail.com>
2015-07-30 09:13:20 +0800

    edit002 add month select

--- js/process/get-available-time.js
+++ js/process/get-available-time.js
@@ -1,43 +1,45 @@
 // JavaScript Document
 
-var time = "03"; // 设置选课日期
+var time = "3"; // 设置选课日期
+var date = "03";
 
 // 跳转到这个礼拜
-toThisWeek();
+openCalendar();
+
+function openCalendar()
+{
+    // 打开日历
+    if (document.querySelector(".calendar") != null) {
+        document.querySelector(".calendar").click();
+        toNextMonth();
+    } else {
+        setTimeout("toThisWeek()", 400);
+    }
+}
+
+function toNextMonth()
+{
+    // TODO 日历要从这个月翻到下个月，并不是每次选课都需要
+    document.getElementsByClassName("selectMonthRight")[0].click();
+    toThisWeek();
+}
 
 function toThisWeek()
 {
-    // 打开日历
-    while (true) {
-        if (document.querySelector(".calendar") != null) {
-            document.querySelector(".calendar").click();
-            break;
-        }
-    }
-
-    // TODO 日历要从这个月翻到下个月，并不是每次选课都需要
-    document.getElementsByClassName("selectMonthRight")[0].click();
-
-
+    //alert("222");
     // 跳转到制定日期 
 	var num = 0;
-    var flag = 0;
-    while (true) {
-        if (document.getElementsByTagName("a") != null) {
-            num = document.getElementsByTagName("a").length;
-        }
-        for (var i = 0; i < num; i++) {
-	        if (document.getElementsByTagName("a")[i].innerHTML == time) {
-		        document.getElementsByTagName("a")[i].click();	
-		        //获取第一个包含“search”类的元素，再点击search
-    		    document.querySelector(".search").click();	
-                flag = 1;
-		        break;
-	        }
-        }
-        if (flag == 1) {
+    if (document.getElementsByTagName("a") != null) {
+        num = document.getElementsByTagName("a").length;
+    }
+    //alert(num);
+    for (var i = 0; i < num; i++) {
+	    if (document.getElementsByTagName("a")[i].innerHTML == time) {
+		    document.getElementsByTagName("a")[i].click();	
+		    //获取第一个包含“search”类的元素，再点击search
+    		document.querySelector(".search").click();
             break;
-        }
+	    }
     }
     // 保存数据
     setTimeout("saveAvaliableTime()", 400);  // 防止search.click还没生效
@@ -49,7 +51,7 @@ function saveAvaliableTime()
     //alert(document.getElementsByClassName("write")[0]);
     if (document.getElementsByClassName("write")[0] === undefined) { // TODO 没设置时间可能出现死循环
         setTimeout("saveAvaliableTime()", 200);
-    } else if (document.getElementsByClassName("ds")[0].innerHTML.slice(0, 2) == time){  // 可能search可能还没有加载完成
+    } else if (document.getElementsByClassName("ds")[0].innerHTML.slice(0, 2) == date){  // 可能search可能还没有加载完成
         // localStorage可以存储json对象
         // 所以我们将设置的所有可用时间设置为json格式
         var Obj = {avaliableTime : []} // 申明一个json对象
@@ -65,7 +67,7 @@ function saveAvaliableTime()
         // 清空obj内容
         //alert(str);
         localStorage.obj = str;
-        //toSchedulePage();
+        toSchedulePage();
     } else {
         document.querySelector(".search").click();
         setTimeout("saveAvaliableTime()", 300);
